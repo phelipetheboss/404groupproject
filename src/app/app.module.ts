a91991207044f96b1dfbc65d51f4ecdb78928578
Dudu Missono <dudumissono@gmail.com>
2022-11-03 19:01:36 -0400

    updated duplicated content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-<<<<<<< HEAD
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProductsComponent } from './pages/products/products.component';
-=======
-import { ContactComponent } from './pages/contact/contact.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { AboutComponent } from './pages/about/about.component';
->>>>>>> Eduardo
+
 
 @NgModule({
   declarations: [
@@ -22,15 +17,9 @@ import { AboutComponent } from './pages/about/about.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-<<<<<<< HEAD
     AboutComponent,
     ContactComponent,
     ProductsComponent
-=======
-    ContactComponent,
-    ProductsComponent,
-    AboutComponent
->>>>>>> Eduardo
   ],
   imports: [
     BrowserModule,
